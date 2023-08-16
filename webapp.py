cf9c154acd1a7a0c214d7a4f6e4ccf78ea7a2bec
Skye725 <101329664+Skye725@users.noreply.github.com>
2023-08-16 20:01:03 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,34 +2,54 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 def main():
+    # Add some space above the title to move it up
+    # st.markdown("<br><br><br>", unsafe_allow_html=True)
+    
+    # Use CSS to style the container
+    st.markdown(
+        """
+        <style>
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 70vh;
+            position: relative;
+        }
+        
+        .content {
+            width: 50%;
+            padding: 20px;
+            border-right: 1px solid #ccc;
+            z-index: 1;
+            background-color: white;
+        }
+        
+        .df-messenger {
+            width: 50%;
+            position: absolute;
+            right: 0;
+            top: 0;
+            z-index: 2;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    
     st.title('Osteoporosis Care Assistant')
-    # Embed Dialogflow Messenger using HTML
-#     st.title("Hello! Here is your Osteoporosis Care Assistant!")
-#     st.write("I am a chatbot that can help you understand and care for osteoporosis. ")
-#     st.write("Click below to start chatting with me!")
-    
-#     html_code ="""
-#     <script src="https://www.gstatic.com/dialogflow-console/fast/messenger-cx/bootstrap.js?v=1"></script>
-#     <df-messenger
-#       df-cx="true"
-#       location="us-central1"
-#       chat-title="Osteoporosis Care Assistant"
-#       agent-id="89b18026-b1f1-4e60-bb15-607bfd386d97"
-#       language-code="en"
-#     ></df-messenger>
-#     """        
-#     st.components.v1.html(html_code, height=300, scrolling=True)
-    
-    
-    html_code ="""
-   <div style="display: flex; justify-content: center; align-items: center; height: 70vh; "> <!-- Adjust the height here -->
-        <div style="width: 50%; padding: 20px; border-right: 1px solid #ccc;">
+
+    html_code = """
+    <div class="container">
+        <div class="content">
             <!-- Your content goes here -->
-            <p style="font-size: 20px;">Hello! Here is your Osteoporosis Care Assistant!</p>
-            <p style="font-size: 20px;">I am a chatbot that can help you understand and care for osteoporosis.</p>
+            <p style="font-size: 24px;">Hello! Here is your Osteoporosis Care Assistant!</p>
+            <br>
+            <p style="font-size: 20px;">I am a chatbot that can help you with osteoporosis.</p>
+            <br>
             <p style="font-size: 20px;">Click below to start chatting with me!</p>
         </div>
-        <div style="width: 50% ;">
+        <div class="df-messenger">
             <script src="https://www.gstatic.com/dialogflow-console/fast/messenger-cx/bootstrap.js?v=1"></script>
             <df-messenger
                 df-cx="true"
@@ -41,10 +61,7 @@ def main():
         </div>
     </div>
     """
-    st.components.v1.html(html_code, height=700, scrolling=False)
-    
-    
-    
+    st.components.v1.html(html_code, height=650, scrolling=False)
+
 if __name__ == "__main__":
     main()
-
