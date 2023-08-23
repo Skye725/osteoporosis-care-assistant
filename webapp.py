0e4becb9c1b95cb24959f699c3b6b56c7a293579
Skye725 <101329664+Skye725@users.noreply.github.com>
2023-08-23 16:48:06 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -63,22 +63,33 @@ def main():
     """
     st.components.v1.html(html_code, height=650, scrolling=False)
     
-    st.write("Please provide your satisfaction rating from 1 to 5:")
+    # st.write("Please provide your satisfaction rating from 1 to 5:")
    
-    survey_questions = [
-            "The assessment result is accurate",
-            "The AI expert can provide accurate answers",
-            "The system is useful",
-            "The system is easy to use",
-            "The system is flexible"
-        ]
+    # survey_questions = [
+    #         "The assessment result is accurate",
+    #         "The AI expert can provide accurate answers",
+    #         "The system is useful",
+    #         "The system is easy to use",
+    #         "The system is flexible"
+    #     ]
 
-    # Loop through each survey question
-    for question in survey_questions:
-        # st.write(f"Please provide your satisfaction rating for the following question: {question}")
+    # # Loop through each survey question
+    # for question in survey_questions:
+    #     # st.write(f"Please provide your satisfaction rating for the following question: {question}")
 
-        # Create a slider for user to input satisfaction rating
-        satisfaction_rating = st.slider(f"{question} (1-5)", min_value=1, max_value=5, step=1)
+    #     # Create a slider for user to input satisfaction rating
+    #     satisfaction_rating = st.slider(f"{question} (1-5)", min_value=1, max_value=5, step=1)
+    st.components.v1.html(
+        """
+        <div data-tf-widget="FmSIrjqJ" data-tf-opacity="100"
+             data-tf-iframe-props="title=User Research Form for Osteoporosis Chatbot App"
+             data-tf-transitive-search-params data-tf-medium="snippet"
+             style="width:100%;height:500px;">
+        </div>
+        <script src="//embed.typeform.com/next/embed.js"></script>
+        """,
+        height=550
+    )
 
 
 
